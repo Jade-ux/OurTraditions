b9b434a4b53263b3d83bea757cade61b5a37676d
Jade-ux <jadeemailsandbox@gmail.com>
2021-02-09 06:37:37 +0000

    Added functionality to edit tradition form for users to edit their images.

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def upload_file():
 
     """
     output = ""
-
+ 
     file = request.files["trad_image"]
 
     if file.filename == "":
@@ -99,7 +99,7 @@ def resize_image(file):
     # Reapply raw_image format so that it can be resized
     new_image.format = saved_format
 
-    # Resize image and set max-length in either axis to 500px
+    # Resize image and set max size on either axis to 500px
     new_image.thumbnail((500, 500))
 
     # Save the image to an in-memory file
@@ -331,49 +331,58 @@ def add_tradition():
 
 @app.route("/edit_tradition/<tradition_id>", methods=["GET", "POST"])
 def edit_tradition(tradition_id):
-    tradition = mongo.db.traditions.find_one(
-                {"_id": ObjectId(tradition_id)})
-    trad_owner = tradition["created_by"]
-    # check if user is logged in, if not flash message and redirect to login 
-    if "user" not in session: 
-        flash("Please log in to edit your traditions")
-        return redirect(url_for("login"))
-    
-    # check if user is the tradition owner
-    if session["user"] != trad_owner:
-        traditions = list(mongo.db.traditions.find())
-        username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
-        flash("You are trying to edit someone else's tradition!")
-        return render_template(
-            "user_profile.html", username=username, traditions=traditions)
+    if request.method == "POST":
+        tradition = mongo.db.traditions.find_one(
+                    {"_id": ObjectId(tradition_id)})
+        trad_owner = tradition["created_by"]
+        # check if user is logged in, if not flash message and redirect to login 
+        if "user" not in session: 
+            flash("Please log in to edit your traditions")
+            return redirect(url_for("login"))
+        
+        # check if user is the tradition owner
+        if session["user"] != trad_owner:
+            traditions = list(mongo.db.traditions.find())
+            username = mongo.db.users.find_one(
+            {"username": session["user"]})["username"]
+            flash("You are trying to edit someone else's tradition!")
+            return render_template(
+                "user_profile.html", username=username, traditions=traditions)
 
-    else: 
-        if request.method == "POST":
-            # collects the form fields and creates a dictionary
-            edit_tradition = {
-                "tradition_name": request.form.get("tradition_name"),
-                "category_name": request.form.get("category_name"),
-                "group_name": request.form.get("group_name"),
-                "country_name": request.form.get("country_name"),
-                "tradition_description": request.form.get("tradition_description"),
-                "created_by": session["user"]
-            }
-            mongo.db.traditions.update(
-                {"_id": ObjectId(tradition_id)}, edit_tradition)
-            flash("Your tradition has been updated.")
+        else: 
+            new_image = request.form.get("trad_image")
+            #if request.files["trad_image"].filename == "":
+            if new_image == "":
+                edited_image = tradition["trad_image"]
 
-        # if method is not POST then revert to this default
-        tradition = mongo.db.traditions.find_one({"_id": ObjectId(tradition_id)})
-        categories = mongo.db.categories.find().sort("category_name", 1)
-        groups = mongo.db.groups.find().sort("group_name", 1)
-        countries = mongo.db.countries.find().sort("country_name", 1)
-        return render_template(
-            "edit_tradition.html",
-            tradition=tradition,
-            categories=categories,
-            groups=groups,
-            countries=countries)
+            else:
+                edited_image = upload_file()
+
+        # collects the form fields and edits the tradition
+        edit_tradition = {
+            "tradition_name": request.form.get("tradition_name"),
+            "category_name": request.form.get("category_name"),
+            "group_name": request.form.get("group_name"),
+            "country_name": request.form.get("country_name"),
+            "tradition_description": request.form.get("tradition_description"),
+            "created_by": session["user"],
+            "trad_image": edited_image,
+        }
+        mongo.db.traditions.update(
+            {"_id": ObjectId(tradition_id)}, edit_tradition)
+        flash("Your tradition has been updated.")
+
+    # if method is not POST then revert to this default
+    tradition = mongo.db.traditions.find_one({"_id": ObjectId(tradition_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    groups = mongo.db.groups.find().sort("group_name", 1)
+    countries = mongo.db.countries.find().sort("country_name", 1)
+    return render_template(
+        "edit_tradition.html",
+        tradition=tradition,
+        categories=categories,
+        groups=groups,
+        countries=countries)
 
 
 @app.route("/delete_tradition/<tradition_id>")
